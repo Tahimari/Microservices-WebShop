ba8de3614571aa6693b801eb89e173ee18545a2a
Tahimari <kamilm215@gmail.com>
2019-06-08 12:20:02 +0200

    Added decode and encode jwt functions

--- users-service-flask/users/project/api/models.py
+++ users-service-flask/users/project/api/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.sql import func
 import datetime
+import jwt
 from project import db
 
 class User(db.Model):
@@ -28,3 +29,28 @@ class User(db.Model):
             'last_name': self.last_name,
             'password': self.password
         }
+
+    def encode_auth_token(self, user_id):
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                "\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0",
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(auth_token, "\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0")
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
