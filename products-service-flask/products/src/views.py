7a761915620d412395cc5d4834f20b2be0eb1490
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-06-08 14:17:07 +0200

    Dodanie widoku change_category_name()

--- products-service-flask/products/src/views.py
+++ products-service-flask/products/src/views.py
@@ -38,3 +38,30 @@ def get_all_categories():
 	responseData['data'] = categoriesSchema.dump(categories).data
 	
 	return jsonify(responseData), 200
+
+@views_blueprint.route('/products/categories/<category_id>', methods=['PUT'])
+def change_category_name(category_id):
+	if not category_id.isdigit():
+		return jsonify({'status' : 'fail', 'message' : "'category_id' must be int!"})
+	
+	requestJSON = request.json
+	
+	if requestJSON is None:
+		errorMessage = 'Cannot find JSON object in request body!'
+		return jsonify({'status' : 'fail', 'message' : errorMessage}), 400
+	
+	categoryToChange = Categories.query.get(category_id)
+	if categoryToChange is None:
+		return jsonify({'status' : 'fail', 'message' : "Category doesn't exist!"}), 404
+	
+	if 'category_name' not in requestJSON.keys():
+		errorMessage = "Cannot find 'category_name' in request body!"
+		return jsonify({'status' : 'fail', 'message' : errorMessage}), 400	
+	
+	try:
+		categoryToChange.name = requestJSON['category_name']
+		db.session.commit()
+		return jsonify({'status' : 'success', 'message' : 'Changed category name!'}), 200
+	except IntegrityError as e:
+		errorInfo = e.orig.args
+		return jsonify({'status' : 'fail', 'message' : errorInfo[0]}), 409
