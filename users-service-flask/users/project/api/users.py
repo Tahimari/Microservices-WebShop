0b257e94f5613fdbfa029101e65645163bfbdfb2
Tahimari <kamilm215@gmail.com>
2019-06-08 15:35:48 +0200

    Added login and logout route (in progress)

--- users-service-flask/users/project/api/users.py
+++ users-service-flask/users/project/api/users.py
@@ -85,6 +85,76 @@ class Users(Resource):
 
 api.add_resource(Users, '/users/<user_id>')
 
+class LoginAPI(Resource):
+    def post(self):
+        post_data = request.get_json()
+        try:
+            user = User.query.filter_by(
+                email=post_data.get('email')
+              ).first()
+            print(user)
+            auth_token = user.encode_auth_token(user.id)
+            if auth_token:
+                responseObject = {
+                    'status': 'success',
+                    'data': {
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token.decode()
+                    }
+                }
+                return responseObject, 200
+        except Exception as e:
+            print(e)
+            responseObject = {
+                'status': 'fail',
+                'data': {
+                    'message': 'Try again'
+                }
+            }
+            return responseObject, 500
+api.add_resource(LoginAPI, '/users/login')
+
+# class LogoutAPI(Resource):
+#     def post(self):
+#         auth_header = request.headers.get('Authorization')
+#         if auth_header:
+#             auth_token = auth_header.split(" ")[1]
+#         else:
+#             auth_token = ''
+#         if auth_token:
+#             resp = User.decode_auth_token(auth_token)
+#             if not isinstance(resp, str):
+#                 return False
+#                 # blacklist_token = BlacklistToken(token=auth_token)
+#                 # try:
+#                 #     insert the token
+#                 #     db.session.add(blacklist_token)
+#                 #     db.session.commit()
+#                 #     responseObject = {
+#                 #         'status': 'success',
+#                 #         'message': 'Successfully logged out.'
+#                 #     }
+#                 #     return responseObject, 200
+#                 # except Exception as e:
+#                 #     responseObject = {
+#                 #         'status': 'fail',
+#                 #         'message': e
+#                 #     }
+#                 #     return responseObject, 200
+#             else:
+#                 responseObject = {
+#                     'status': 'fail',
+#                     'message': resp
+#                 }
+#                 return responseObject, 401
+#         else:
+#             responseObject = {
+#                 'status': 'fail',
+#                 'message': 'Provide a valid auth token.'
+#             }
+#             return responseObject, 403
+# api.add_resource(LogoutAPI, '/users/logout')
+
 @users_blueprint.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
