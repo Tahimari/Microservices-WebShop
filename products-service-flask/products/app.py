787f79001b984e1d34274a8e7ecce7d34ffa2fb6
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-06-08 11:17:54 +0200

    Dodanie pliku config.py

--- products-service-flask/products/app.py
+++ products-service-flask/products/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from src.config import APP_DEBUG_MODE, APP_HOST_ADDRESS, APP_RUNNING_PORT
 
 # Init app
 app = Flask(__name__)
@@ -8,4 +9,4 @@ def hello():
 	return jsonify({"response" : "ping ping pong!"})
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=80)
+	app.run(debug=APP_DEBUG_MODE, host=APP_HOST_ADDRESS, port=APP_RUNNING_PORT)
