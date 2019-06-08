d334031b2965468ef7fb4e9e51f6c30d235636c2
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-06-08 15:59:50 +0200

    Poprawa inicjalizacji bazy danych

--- products-service-flask/products/src/utility_functions.py
+++ products-service-flask/products/src/utility_functions.py
@@ -4,10 +4,15 @@ from src.config import DATABASE_DIR_PATH
 
 def doesDatabaseExist(pathToDatabase):
 	return os.path.isfile(pathToDatabase)
-	
+
 def prepare_database():
 	if not os.path.exists(DATABASE_DIR_PATH):
 		os.mkdir(DATABASE_DIR_PATH)
 	
 	db.create_all()
+	db.session.add(Categories('Shoes'))
+	db.session.add(Categories('T-Shirts'))
+	db.session.add(Categories('Jackets'))
+	db.session.add(Categories('Trousers'))
+	db.session.add(Categories('Shirts'))
 	db.session.commit()
