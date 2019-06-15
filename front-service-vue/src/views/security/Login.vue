b8046c018de6f31a578057fac3d86d95fe67c68e
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-06-15 11:49:37 +0200

    Poprawa wyglądu strony logowania

--- front-service-vue/src/views/security/Login.vue
+++ front-service-vue/src/views/security/Login.vue
@@ -2,20 +2,21 @@
     <div class="container">
         <b-jumbotron header="Please sign in">
             <Alert v-if="alert" v-bind:message="alert"/>
-            <form v-on:submit="login" class="form-signin" method="post">
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" value="" name="email" id="inputEmail" class="form-control"
-                       placeholder="Email address" required autofocus v-model="pass.email">
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" name="password" id="inputPassword" class="form-control"
-                       placeholder="Password" v-model="pass.password">
-    <!--            <div class="checkbox mb-3">-->
-    <!--                <label>-->
-    <!--                    <input type="checkbox" name="_remember_me"> Remember me-->
-    <!--                </label>-->
-    <!--            </div>-->
-                <button type="submit" class="btn btn-primary">Log in!</button>
-             </form>
+            <!-- Login form -->        
+            <b-form @submit="login" class="w-100" method="post">
+                <!-- Email -->
+				<b-form-group id="form-email-group" label="Email:" label-for="form-email-input">
+					<b-form-input id="form-email-input" type="email" v-model="pass.email" required autofocus placeholder="Enter email">
+					</b-form-input>
+				</b-form-group>
+                <!-- Password -->
+				<b-form-group id="form-password-group" label="Password:" label-for="form-password-input">
+					<b-form-input id="form-password-input" type="password" v-model="pass.password" required placeholder="Enter password">
+					</b-form-input>
+				</b-form-group>
+                <!-- Submit button -->
+				<b-button type="submit" variant="primary">Log in!</b-button>
+            </b-form>
             <router-link to="/register">Don't have account? Sign up!</router-link>
         </b-jumbotron>
     </div>
